7f15724e8f694947df008b90c559ecd2e96d1587
mizutani-coa <mizutani@coamix.co.jp>
2023-02-17 15:05:22 +0900

    fix: header完成

--- src/pages/imas-test.tsx
+++ src/pages/imas-test.tsx
@@ -1,7 +1,37 @@
 export default function imas() {
   return (
     <>
-      <div className='relative mx-2.5 mt-2 box-content inline-block h-16 w-40 bg-gray-800'></div>
+      <div className='m-auto flex h-full max-w-full items-center justify-between'>
+        <div className='relative mx-2.5 mt-2 box-content inline-block h-16 w-40 justify-center bg-gray-500 text-center align-middle'>
+          LOGO
+        </div>
+        <div className='relative flex items-center justify-end'>
+          <div className='relative flex items-center justify-center'>
+            <div className='box-content inline-block h-16 w-20 bg-gray-50 text-center align-middle'>
+              ticket
+            </div>
+          </div>
+          <div className='relative flex items-center justify-center'>
+            <div className='box-content inline-block h-16 w-20 bg-gray-50 text-center align-middle'>
+              store
+            </div>
+          </div>
+          <div className='relative flex items-center justify-center'>
+            <div className='box-content inline-block h-16 w-20 bg-gray-50 text-center align-middle'>
+              search
+            </div>
+          </div>
+        </div>
+        <div className='tx-sm relative ml-20 flex items-center justify-center rounded-3xl bg-gray-500 px-20 py-4 align-middle font-bold text-pink-400 shadow-md '>
+          login
+        </div>
+      </div>
+
+      <div className='relative w-full overflow-hidden'>
+        <div className='w-630px pb-30px  relative m-auto block'>
+          <div className='inline-box box-content h-full bg-gray-500'></div>
+        </div>
+      </div>
     </>
   );
 }
