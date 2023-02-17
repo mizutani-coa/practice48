aa568acf7c74ca833d0e9d305a6d98e851580320
mizutani-coa <mizutani@coamix.co.jp>
2023-02-17 18:26:44 +0900

    fix: ボタン作成

--- src/pages/imas-test.tsx
+++ src/pages/imas-test.tsx
@@ -27,11 +27,76 @@ export default function imas() {
         </div>
       </div>
 
-      <div className='relative w-full overflow-hidden'>
-        <div className='w-630px pb-30px  relative m-auto block'>
-          <div className='inline-box box-content h-full bg-gray-500'></div>
+      <div className='static m-0 my-10 box-border flex h-48 w-full p-0'>
+        <div className='mx-4 box-content inline-block h-full w-60 justify-center bg-gray-500 text-center align-middle'>
+          box
+        </div>
+        <div className='mx-4 box-content inline-block h-full w-60 justify-center bg-gray-500 text-center align-middle'>
+          box
+        </div>
+        <div className='mx-4 box-content inline-block h-full w-60 justify-center bg-gray-500 text-center align-middle'>
+          box
+        </div>
+        <div className='mx-4 box-content inline-block h-full w-60 justify-center bg-gray-500 text-center align-middle'>
+          box
         </div>
       </div>
+
+      <div className='mx-1 box-border flex w-full grid-cols-6'>
+        <div className='h-20 w-1/6 bg-pink-100 text-center'>
+          <span>
+            BRAND <br />
+            SUMMARY
+            <br />
+          </span>
+          <span>ブランド情報</span>
+        </div>
+        <div className='mx-2 h-20 w-1/6 bg-pink-500 text-center'>
+          THE IDOLM@STER
+        </div>
+        <div className='mx-2 h-20 w-1/6 bg-pink-500 text-center'>
+          シンデレラガールズ
+        </div>
+        <div className='mx-2 h-20 w-1/6 bg-pink-500 text-center'>
+          ミリオンライブ
+        </div>
+        <div className='mx-2 h-20 w-1/6 bg-pink-500 text-center'>SideM</div>
+        <div className='mx-2 h-20 w-1/6 bg-pink-500 text-center'>
+          シャイニーカラーズ
+        </div>
+      </div>
+
+      <div className='mx-1 my-2 box-border flex grid w-full grid-cols-6'>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>ニュース</div>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>ゲーム</div>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>
+          ライブ・イベント
+        </div>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>アニメ</div>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>配信番組</div>
+        <div className='h-20 w-11/12 bg-blue-300 text-center '>ラジオ</div>
+
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>グッズ</div>
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>
+          コラボ・キャンペーン
+        </div>
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>
+          ミュージック
+        </div>
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>
+          ブック・コミック
+        </div>
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>
+          メディア
+        </div>
+        <div className='my-2 h-20 w-11/12 bg-blue-300 text-center'>
+          ムービー
+        </div>
+      </div>
+
+      <div className='box-border h-14 bg-gray-100'>NEWS</div>
+
+      <div className='mx-1 my-2 box-border flex grid w-full grid-cols-3'></div>
     </>
   );
 }
